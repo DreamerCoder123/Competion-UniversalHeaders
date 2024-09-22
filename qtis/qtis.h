53ba1d60170730924e7d1f0334b372a1b34558f8
lihaozhe <w18942020649@163.com>
2024-09-22 21:29:45 +0800

    修改了qtis中的部分逻辑并且添加了注释

--- qtis/qtis.h
+++ qtis/qtis.h
@@ -17,32 +17,35 @@ qtis::qtis(unsigned char pin, unsigned char weight)
 }
 short int qtis::getWeight()
 {
-    return (digitalRead(this->pin)) * this->weight; // 返回加权数
+    return (!digitalRead(this->pin)) * this->weight; // 返回加权数
 }
-class New_qtis
+
+class muti_qti
 {
 public:
-    New_qtis(qtis *qti);
+    muti_qti(qtis *qti, short int length);
     short int _qtiread();
 
 private:
     qtis qti[4];
     const short int PINS_MAX = 4;
+    short int length; // 存储数组的长度
 };
-New_qtis::New_qtis(qtis *qti)
+muti_qti::muti_qti(qtis *qti, short int length)
 {
+    this->length = length; // 长度初始化
     for (int i = 0; i < PINS_MAX; i++)
     {
         this->qti[i] = qti[i]; // 对所有的qti进行初始化
     }
 }
-short int New_qtis::_qtiread()
+short int muti_qti::_qtiread()
 {
     // 统一以向右为正
     short int total = 0;
-    for (int i = 0; i < this->PINS_MAX; i++)
+    for (int i = 0; i < this->length; i++)
     {
         total += this->qti[i].getWeight();
-    }
+    }//!读取所有的加权数
     return total;
 }
