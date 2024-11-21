28938ea3f9817aaf2dcdd38ac2074727ea8968c6
lihaozhe <w18942020649@163.com>
2024-11-21 19:04:42 +0800

    优化代码逻辑，添加时间变量以跟踪状态；重构捕捉和对齐功能，简化；处理书框检测及书抓取的逻辑，提升代码读性和功能性。

--- UniversalHeaders.ino
+++ UniversalHeaders.ino
@@ -2,7 +2,8 @@
 #include "Constants.h"
 #include "huskyControl.h"
 #define DEBUGGING false
-#define point true
+long long current_timec = millis();
+long long current_time_husky = millis();
 void setup()
 {
     Serial.begin(115200);                            // HardwareSerial  init
@@ -16,87 +17,120 @@ void setup()
         arms.QtConnection();                      // 启用于pythonQt的链接
         Serial.println("Warning Debugging Mode"); // 打印提示信息
     }
-    while (point)
+}
+bool while_catch(short int id)
+{
+    if (husky_related::detectID(id))
     {
-        Arm_opreations::Chan_book::chanbook_left();
-        // arms.insistace(1500);
-        // Arm_opreations::Catch_Book::catchbook(1);
-        // arms.insistace(1000);
-        // Arm_opreations::Catch_Book::catchbook(2);
-        // arms.insistace(3000);
-        // Arm_opreations::Chan_book::chanbook(5);
-        // Arm_opreations::Chan_book::release_book();
-        // Arm_opreations::Chan_book::chanbook(6);
-        // Arm_opreations::Chan_book::release_book();
+        wheels.run(RUN_TOWARDS);
+        while (true)
+        {
+            if (((id) == 1) ? (r_num != 2) : (l_num != 2)) // 自动检测
+            {
+                while (!husky_related::cooperate::avaiblility_check(id))
+                {
+                    long long current_time = millis();
+                    current_timec = current_time;
+                    while (!husky_related::detectID(id))
+                    {
+                        // 如果丢失了目标
+                        if (millis() - current_time > 2000)
+                        {
+
+                            // 丢失目标超过两秒
+                            return false;
+                        }
+                    }
+                }
+                // 对齐完毕
+                Arm_opreations::Catch_Book::catchbook(id);
+                return true;
+            }
+            else
+            {
+                return false;
+                husky.customText("No empty book space", 0, 0);
+            }
+        }
     }
 }
-// ID1,2
+void while_chan(short int id)
+{
+    char Bookshelf = (id == 1) ? 7 : 6;
+    // 如果检测到放置id=2的整齐书框
+    if ((Bookshelf == 7 ? r_num : l_num) != 0)
+    {
+
+        // 如果左边或右边有书
+        long long current_time = millis();
+        while (!husky_related::cooperate::avaiblility_check(Bookshelf))
+        {
+            if (current_time - millis() > 2000)
+            {
+                // 丢失目标超过两秒
+                return;
+            }
+        }
+        // 对齐完毕
+        Arm_opreations::Chan_book::chanbook(Bookshelf);
+    }
+    else
+    {
+        qti_related::qti_run();
+        husky.customText("No ok book for " + String(Bookshelf), 0, 0);
+    }
+}
+
 void loop()
 {
     qti_related::qti_run();
     if (husky_related::detectID(6))
     {
         wheels.run(RUN_TOWARDS);
-        //检测到放置id=2的大书框
+        // 检测到放置id=2的大书框
     }
-    if (husky_related::detectID(1))
+    if (husky_count > 0)
     {
-        wheels.run(RUN_TOWARDS);
-        while (true)
+        current_time_husky = millis();
+    }
+    // 速度控制
+    if (millis() - current_time_husky > 3000)
+    {
+        // 3秒没有检测到标签，提升速度
+        speedLeft = 75;
+        speedRight = 75;
+    }
+    else
+    {
+        speedLeft = 85;
+        speedRight = 85;
+    }
+    current_timec = millis();
+    if (husky_related::detectID(5))
+    {
+        while (millis() - current_timec < 4000 && (l_num != 2 || r_num != 2))
         {
-            if (r_num <= 1)
-            {
-                while (!husky_related::cooperate::avaiblility_check(1))
-                {
-                    long long current_time = millis();
-                    while (!husky_related::detectID(1))
-                    {
-                        // 如果丢失了目标
-                        if (millis() - current_time > 2000)
-                        {
-                            // 丢失目标超过两秒
-                            return;
-                        }
-                    }
-                }
-                // 对齐完毕
-                Arm_opreations::Catch_Book::catchbook(1);
-                break;
-            }
-            else
-            {
-                break;
-            }
+            qti_related::qti_run();
+            while_catch(1);
+            while_catch(2);
+            // 检测到乱书框内的书
+            current_timec = millis();
+            // 抓取1，2号书
+        }
+        // 如果在5秒内没有检测到目标，则放弃抓取
+    }
+    if (husky_related::detectID(6))
+    {
+        while (millis() - current_timec < 5000)
+        {
+            while_chan(2);
         }
     }
-    if (husky_related::detectID(2))
+    if (husky_related::detectID(7))
     {
-        wheels.run(RUN_TOWARDS);
-        while (true)
+        while (millis() - current_timec < 5000)
         {
-            if (l_num <= 1)
-            {
-                while (!husky_related::cooperate::avaiblility_check(2))
-                {
-                    long long current_time = millis();
-                    while (!husky_related::detectID(1))
-                    {
-                        // 如果丢失了目标
-                        if (millis() - current_time > 2000)
-                        {
-                            // 丢失目标超过两秒
-                            return;
-                        }
-                    }
-                }
-                // 对齐完毕
-                Arm_opreations::Catch_Book::catchbook(2);
-                break;
-            }
-            else
-            {
-                break;
-            }
+            while_chan(1);
         }
     }
 }
